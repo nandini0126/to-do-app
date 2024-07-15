d258358d21b1f7419f01b9bd5617d1fe56896ace
nandini0126 <nandiniyamale26@gmail.com>
2024-07-15 20:29:23 +0530

    -

--- src/components/TodoCard/TodoCard.js
+++ src/components/TodoCard/TodoCard.js
@@ -1,31 +1,8 @@
+import React from 'react'
 
-import "./TodoCard.css"
-import DelImg from "./delete.png"
-
-function TodoCard({index, task, category,deleteItem}) {
-
-
-  const CATEGORY_EMOJI={
-    Learning: "📚",
-    Work: "💼",
-    Personal: "🏡",
-    Shopping: "🛒",
-    Health: "🏥",
-    Others: "🗂️"
-  }
-
+function TodoCard() {
   return (
-    <div className="item-card">
-      <img src={DelImg} alt="img" className="del-img" 
-      onClick={()=>{
-        deleteItem(index)
-        }}/>
-        {task} 
-        
-        <span className="category-badge">
-        {CATEGORY_EMOJI[category]} 
-        {category}</span>
-    </div>
+    <div>TodoCard</div>
   )
 }
 
